151b8dc9691a81ad70e48f0850e9ef59602f3923
DESKTOP-Q75A6S2\USER <anhhuynh98@gmail.com>
2016-12-08 23:02:51 +0700

    Homepage without thumbail

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+ <ul id="topbar_ul">
+  <span>
+   	<li id="topbar"><a href="index.php"> HOME </a></li>
+  	<li id="topbar"><a href="admin/login.php"> . WRITE! </a></li>
+  </span>
+  <span style=" width:300px;
+ 			    padding-top:5px;
+  				border-style:solid;
+  				border-color:#FFFFFF;
+  				border-width:1px;
+  				padding-left:10px;
+  				padding-bottom:5px;
+  				padding-right: 10px;
+  				border-radius: 25px;
+  				font-style: bold;
+  				color:#FFFFFF;
+  				">
+		NUS CVWO Assignment 1
+</span>
+ </ul> 
+
 <?php 
 /*
 The index file will list all posts from the posts table.
@@ -10,30 +31,34 @@ require('includes/config.php'); ?>
 <head>
     <meta charset="utf-8">
     <title>Anh's Blog</title>
-    <link rel="stylesheet" href="style/normalize.css">
+ 	<link rel="stylesheet" href="style/normalize.css">
     <link rel="stylesheet" href="style/main.css">
 </head>
 <body>
 
 	<div id="wrapper">
-
-		<h1>NUS CVWO Assignment 1 - Anh's Blog</h1>
-		<h2>Not just another WordPress site</h2>
-		<hr />
-
+		<h1>
+  			<span>Anh's</span><span style="color:#F7B731; font-family: Georgia;">Blog</span>
+		</h1>
 		<?php
 			try {
 
 				$stmt = $db->query('SELECT postID, postTitle, postDesc, postDate FROM blog_posts ORDER BY postID DESC');
 				while($row = $stmt->fetch()){
 					
-					echo '<div>';
+					echo '<div id="home_post">';
 						echo '<h1><a href="viewpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h1>';
-						echo '<p>Posted on '.date('jS M Y H:i:s', strtotime($row['postDate'])).'</p>';
+						echo '<p style="font-size:0.9em;"> Posted on '.date('jS M Y H:i:s', strtotime($row['postDate'])).'</p>';
 						echo '<p>'.$row['postDesc'].'</p>';				
-						echo '<p><a href="viewpost.php?id='.$row['postID'].'">Read More</a></p>';				
+						echo '<p><a href="viewpost.php?id='.$row['postID'].'">  
+							<div id="readmore_box">
+								Continue Reading
+							</div>
+						 </a></p>';				
+						echo '<br>';
+						echo '<hr class="style12">';
+						echo '<br>';
 					echo '</div>';
-
 				}
 
 			} catch(PDOException $e) {
@@ -42,6 +67,7 @@ require('includes/config.php'); ?>
 		?>
 
 	</div>
-	<a href="admin/Login.php">+ Login</a><br/>
 </body>
 </html>
+
+
